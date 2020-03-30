7dca1cf9210187622c15b15e5ffca66d3d1d724b
TolstovM <maksfox0@gmail.com>
2020-03-30 12:10:06 +0300

    Operators and, or, not, ==, >, >= added

--- vm/machine/machine.py
+++ vm/machine/machine.py
@@ -47,6 +47,12 @@ class Machine:
         instruction_table.insert(Instruction('SUB', 0, cls.sub))
         instruction_table.insert(Instruction('MUL', 0, cls.mul))
         instruction_table.insert(Instruction('DIV', 0, cls.div))
+        instruction_table.insert(Instruction('AND', 0, cls.and_op))
+        instruction_table.insert(Instruction('OR', 0, cls.or_op))
+        instruction_table.insert(Instruction('NOT', 0, cls.not_op))
+        instruction_table.insert(Instruction('ISEQ', 0, cls.is_equal))
+        instruction_table.insert(Instruction('ISGT', 0, cls.is_greater))
+        instruction_table.insert(Instruction('ISGE', 0, cls.is_greater_equal))
         return instruction_table
 
     def push(self, args):
@@ -80,3 +86,32 @@ class Machine:
         rh = self.pop_operand()
         lh = self.pop_operand()
         self.push_operand(lh / rh)
+
+    def and_op(self, args):
+        rh = self.pop_operand()
+        lh = self.pop_operand()
+        self.push_operand(lh and rh)
+
+    def or_op(self, args):
+        rh = self.pop_operand()
+        lh = self.pop_operand()
+        self.push_operand(lh or rh)
+
+    def not_op(self, args):
+        op = self.pop_operand()
+        self.push_operand(not op)
+
+    def is_equal(self, args):
+        rh = self.pop_operand()
+        lh = self.pop_operand()
+        self.push_operand(lh == rh)
+
+    def is_greater(self, args):
+        rh = self.pop_operand()
+        lh = self.pop_operand()
+        self.push_operand(lh > rh)
+
+    def is_greater_equal(self, args):
+        rh = self.pop_operand()
+        lh = self.pop_operand()
+        self.push_operand(lh >= rh)
